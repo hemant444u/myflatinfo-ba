89242363f2d77809e68101da7c9f07add4f6392f
hemant444u <krguptahemant@gmail.com>
2025-05-21 17:24:13 +0530

    ddd

--- app/Models/BuildingFacility.php
+++ app/Models/BuildingFacility.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Cache;
 
 class BuildingFacility extends Model
 {

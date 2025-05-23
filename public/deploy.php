6817caab8c54209effcdf84dd62a9e50f1e03a18
hemant444u <krguptahemant@gmail.com>
2025-05-24 00:51:04 +0530

    fff

--- public/deploy.php
+++ public/deploy.php
@@ -13,12 +13,7 @@ if (!hash_equals($hash, $signature)) {
 }
 
 // Run git commands
-$cmd = <<<CMD
-cd /var/www/html/myflatinfo-ba && \
-git reset --hard HEAD && \
-git pull origin main
-CMD;
-
+$cmd = 'cd /var/www/html/myflatinfo-ba && git pull origin main';
 $output = shell_exec($cmd);
 
 file_put_contents($logPath, "[".date('Y-m-d H:i:s')."] Webhook triggered\nCMD: $cmd\nOutput:\n$output\n\n", FILE_APPEND);

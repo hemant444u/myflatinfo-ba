420dc3ce6a34832070dfe21c2a2b4fce67f71596
hemant444u <krguptahemant@gmail.com>
2025-05-24 19:09:04 +0530

    fff

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -1638,7 +1638,7 @@ class CustomerController extends Controller
         $item_amount = $essential_payment->dues_amount;
 
         $orderData = [
-            'receipt'         => $item_number,
+            'receipt'         => (string) $item_number,
             'amount'          => $item_amount * 100, // 2000 rupees in paise
             'currency'        => 'INR',
             'payment_capture' => 1 // auto capture

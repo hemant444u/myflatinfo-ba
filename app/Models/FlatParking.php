3982e654c3220118fa5623e05956628f76943c20
hemant444u <krguptahemant@gmail.com>
2025-05-29 16:57:48 +0530

    ddd

--- app/Models/FlatParking.php
+++ app/Models/FlatParking.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Flat extends Model
+class FlatParking extends Model
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory;
     
     public function flat()
     {

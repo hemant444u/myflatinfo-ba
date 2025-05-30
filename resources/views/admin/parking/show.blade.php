ae7da96e53decac12d99a73c98351b1d6a1df4d4
hemant444u <krguptahemant@gmail.com>
2025-05-30 12:55:30 +0530

    fff

---
+++ resources/views/admin/parking/show.blade.php
@@ -0,0 +1,325 @@
+@extends('layouts.admin')
+
+@section('title')
+    Parking Details
+@endsection
+
+@section('content')
+
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Parking Details</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Parking Details</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-3">
+
+            <!-- Profile Image -->
+            <div class="card card-primary card-outline">
+              <div class="card-body box-profile">
+                <div class="text-center">
+                <h3 class="profile-username text-center">{{$parking->name}}</h3>
+                </div>
+                <ul class="list-group list-group-unbordered mb-3">
+                  <li class="list-group-item">
+                    <b>Building</b> <a class="float-right">{{$parking->building->name}}</a>
+                  </li>
+                  <li class="list-group-item">
+                    <b>Block</b> <a class="float-right">{{$parking->block->name}}</a>
+                  </li>
+                  <li class="list-group-item">
+                    <b>Flats</b> <a class="float-right">{{$parking->flats->count()}}</a>
+                  </li>
+                  
+                  <li class="list-group-item">
+                    <b>Status</b> <a class="float-right">
+                        <input type="checkbox" name="my-checkbox" class="status" data-id="{{$parking->id}}" data-bootstrap-switch data-on-text="Active" 
+                        data-off-text="Inactive" {{$parking->status == 'Active' ? 'checked' : ''}}>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+          </div>
+          <!-- /.col -->
+          <div class="col-md-9">
+                <div class="">
+                    @if(session()->has('error'))
+                    <div class="alert alert-danger">
+                        {{ session()->get('error') }}
+                    </div>
+                    @endif
+                    @if(session()->has('success'))
+                    <div class="alert alert-success">
+                        {{ session()->get('success') }}
+                    </div>
+                    @endif
+                </div>
+            <div class="card">
+              <div class="card-header p-2">
+                <ul class="nav nav-pills">
+                  <li class="nav-item"><a class="nav-link active" href="#flats" data-toggle="tab">Flats</a></li>
+                </ul>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content">
+                  <div class="active tab-pane" id="flats">
+                      <button class="btn btn-sm btn-success" data-toggle="modal" data-target="#addModal">Add New Flat</button>
+                    <div class="table-responsive">
+                    <table class="table table-bordered table-striped">
+                      <thead>
+                          <tr>
+                              <th>S No</th>
+                              <th>Building</th>
+                              <th>Block</th>
+                              <th>Flat</th>
+                              <th>Owner</th>
+                              <th>Tanent</th>
+                              <th>Action</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                        <?php $i = 0; ?>
+                        @forelse($parking->flats as $flat)
+                        <?php $i++ ?>
+                        <tr>
+                          <td>{{$i}}</td>
+                          <td>{{$flat->building->name}}</td>
+                          <td>{{$flat->block->email}}</td>
+                          <td>{{$flat->name}}</td>
+                          <td>{{$flat->owner ? $flat->owner->name : ''}}</td>
+                          <td>{{$flat->tanent ? $flat->tanent->name : ''}}</td>
+                          <td>
+                                <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteModal" data-id="{{$flat->pivot->id}}" data-action="delete"><i class="fa fa-trash"></i></button>
+                          </td>
+                        </tr>
+                        @empty
+                        @endforelse
+                      </tbody>
+                    </table>
+                    </div>
+                  </div>
+                  <!-- /.tab-pane -->
+                  
+                </div>
+                <!-- /.tab-content -->
+              </div><!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+    
+<!-- Add Modal -->
+
+<div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add New Flat</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="{{url('store-parking-flat')}}" method="post" class="add-form">
+        @csrf
+        <div class="modal-body">
+          
+          <div class="form-group">
+            <label for="flat" class="col-form-label">Flat No:</label>
+            <div class="input-group">
+              <input type="text" name="flat" class="form-control" id="flat" maxlength="40" placeholder="Flat name or number" required>
+              <div class="input-group-append">
+                <button type="button" class="btn btn-primary" id="getFlatData">Get Flat Data</button>
+              </div>
+            </div>
+          </div>
+          <div class="error text-danger"></div>
+          <div class="form-group">
+            <label for="flat" class="col-form-label">Flat Name:</label>
+            <input type="text" name="flat_name" class="form-control" id="flat_name" disabled required>
+          </div>
+
+          
+          <input type="hidden" name="id" id="edit-id">
+          <input type="hidden" name="user_id" id="user_id">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary" id="save-button">Save</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- Delete Modal -->
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Are you sure ?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p class="text"></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal" id="delete-button">Confirm Delete</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+@section('script')
+
+
+<script>
+  $(document).ready(function(){
+    var id = '';
+    var action = '';
+    var token = "{{csrf_token()}}";
+    
+    $('#deleteModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget);
+      id = button.data('id');
+      $('#delete-id').val(id);
+      action= button.data('action');
+      $('#delete-button').removeClass('btn-success');
+      $('#delete-button').removeClass('btn-danger');
+      $('.modal-title').text('Are you sure ?');
+      if(action == 'delete'){
+          $('#delete-button').addClass('btn-danger');
+          $('#delete-button').text('Confirm Delete');
+          $('.text').text('You are going to permanently delete this item..');
+      }else{
+          $('#delete-button').addClass('btn-success');
+          $('#delete-button').text('Confirm Restore');
+          $('.text').text('You are going to restore this item..');
+      }
+    });
+
+    $(document).on('click','#delete-button',function(){
+      var url = "{{url('delete-parking-flat','')}}";
+      $.ajax({
+        url : url,
+        type: "POST",
+        data : {'_token':token,'id':id,'action':action},
+        success: function(data)
+        {
+          window.location.reload();
+        }
+      });
+    });
+    
+    $('.status').bootstrapSwitch('state');
+        $('.status').on('switchChange.bootstrapSwitch',function () {
+            var id = $(this).data('id');
+            $.ajax({
+                url : "{{url('update-guard-status')}}",
+                type: "post",
+                data : {'_token':token,'id':id,},
+                success: function(data)
+                {
+                  //
+                }
+            });
+        });
+
+    $('#addModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget);
+      var edit_id = button.data('id');
+      $('#edit-id').val(edit_id);
+      $('#flat_name').val(button.data('flat_name'));
+      $('#flat').val(button.data('flat'));
+      $('#user_id').val(button.data('user_id'));
+      $('.modal-title').text('Add New Payment');
+      if(edit_id){
+          $('.modal-title').text('Update Payment');
+      }
+    });
+    
+    $('.status').bootstrapSwitch('state');
+        $('.status').on('switchChange.bootstrapSwitch',function () {
+            var id = $(this).data('id');
+            $.ajax({
+                url : "{{url('update-event-status')}}",
+                type: "post",
+                data : {'_token':token,'id':id,},
+                success: function(data)
+                {
+                  //
+                }
+            });
+        });
+        
+    $('.add-form').on('submit', function (event) {
+      if ($('#flat_name').val().trim() === '') {
+        event.preventDefault();
+        $('.error').text('Flat Name is required. Please fetch flat data.');
+      }
+    });
+    
+    // Fetch user data when clicking "Get User Data"
+    $('#getFlatData').on('click', function () {
+      var flat = $('#flat').val().trim();
+      if (flat === '') {
+        $('.error').text('Please enter an flat to fetch user data.');
+        return;
+      }
+      
+      $('.error').text(''); // Clear previous errors
+      
+      $.ajax({
+        url: '{{ url("get-flat") }}', // Update with your actual route
+        type: 'POST',
+        data: { _token:token,email: email },
+        success: function (response) {
+          if (response.success) {
+            $('#flat_name').val(response.data.name);
+            $('#flat_id').val(response.data.id);
+          } else {
+            $('.error').text('User not found.');
+            $('#flat_name').val('');
+          }
+        },
+        error: function () {
+          $('.error').text('Error fetching flat data.');
+          $('#flat_name').val('');
+        }
+      });
+    });
+
+  });
+</script>
+@endsection
+
+@endsection
+
+
+

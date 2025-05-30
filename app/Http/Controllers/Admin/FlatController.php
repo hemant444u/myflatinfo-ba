6ecb211997bb3d37324fa886bffb50688672c96f
hemant444u <krguptahemant@gmail.com>
2025-05-30 13:38:04 +0530

    ddd

--- app/Http/Controllers/Admin/FlatController.php
+++ app/Http/Controllers/Admin/FlatController.php
@@ -197,6 +197,7 @@ class FlatController extends Controller
             'flat_id' => 'required|exists:flats,id',
             'flat' => 'required|exists:flats,name',
             'parking_id' => 'required|exists:parkings,id',
+            'id' => 'required|exists:parking_flats,id',
         ];
     
         $msg = 'Parking added successfully';

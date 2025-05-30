b7aa57d741d92420b6d8600ee7755e7d6e28bc5d
hemant444u <krguptahemant@gmail.com>
2025-05-30 16:55:09 +0530

    ddd

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -1188,7 +1188,7 @@ class CustomerController extends Controller
             ], 422);
         }
         $visitor = Visitor::where('id',$request->visitor_id)->withTrashed()->first();
-        $visitor_inouts = VisitorInout::where('visitor_id'.$visitor->id)->get();
+        $visitor_inouts = VisitorInout::where('visitor_id',$visitor->id)->get();
         foreach($visitor_inouts as $visitor_inout){
             $visitor_inout->forceDelete();
         }

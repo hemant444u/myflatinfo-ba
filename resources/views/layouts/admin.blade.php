a9e790650a763cefdd66b29a1e83f2daad6e9331
hemant444u <krguptahemant@gmail.com>
2025-06-03 20:26:37 +0530

    fff

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -184,6 +184,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
+              <i class="nav-icon fa fa-bar-chart"></i>
               <p>Accounts<i class="right fas fa-angle-left"></i></p>
             </a>
             <ul class="nav nav-treeview">

59a82b6ff333e31ee6e3e96ea9ab7c51cc599765
hemant444u <krguptahemant@gmail.com>
2025-06-04 00:07:01 +0530

    ddd

--- resources/views/partials/model_data.blade.php
+++ resources/views/partials/model_data.blade.php
@@ -1,6 +1,8 @@
+        <div class="model-id">
             <select name="model_id" class="form-control" id="model_id" required>
                 @forelse($results as $data)
                 <option value="{{$data->id}}">{{$data->name}}</option>
                 @empty
                 @endforelse
             </select>
+        </div>

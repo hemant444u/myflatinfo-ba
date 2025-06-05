0621abd923349f5a4bad8d6f8b236af41dbc6167
hemant444u <krguptahemant@gmail.com>
2025-06-05 20:43:13 +0530

    ddd

--- resources/views/partials/events.blade.php
+++ resources/views/partials/events.blade.php
@@ -3,7 +3,7 @@
             <label for="name" class="col-form-label">Events:</label>
             <select name="model_id" class="form-control" id="model_id" required>
                 @forelse($events as $event)
-                <option value="{{$event->id}}">{{$event->name}}</option>
+                <option value="{{$event->id}}" {{ request('model_id') == $event->id ? 'selected' : ''}}>{{$event->name}}</option>
                 @empty
                 @endforelse
             </select>

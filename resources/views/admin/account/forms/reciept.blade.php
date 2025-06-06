4307369606a33049000b5c3ed295763aebe68ff3
hemant444u <krguptahemant@gmail.com>
2025-06-06 17:53:13 +0530

    ddD

--- resources/views/admin/account/forms/reciept.blade.php
+++ resources/views/admin/account/forms/reciept.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('title')
-    Expenditure Form 
+    Miscellaneous Receivables Form
 @endsection
 
 @section('content')
@@ -11,12 +11,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Expenditure Form</h1>
+            <h1>Miscellaneous Receivables Form</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Expenditure Form</li>
+              <li class="breadcrumb-item active">Miscellaneous Receivables Form</li>
             </ol>
           </div>
         </div>

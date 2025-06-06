8d5d21c4af85f12ceb9d0fa2d1740c3c5c20eae2
hemant444u <krguptahemant@gmail.com>
2025-06-06 16:04:00 +0530

    ddD

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Facility;
+use App\Models\BuildingFacility;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Cache;
 use \Auth;

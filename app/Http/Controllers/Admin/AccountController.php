e9c4bb420ca893fbf5191bf052e99c6cddd2541e
hemant444u <krguptahemant@gmail.com>
2025-06-07 21:34:00 +0530

    fff

--- app/Http/Controllers/Admin/AccountController.php
+++ app/Http/Controllers/Admin/AccountController.php
@@ -86,7 +86,7 @@ class AccountController extends Controller
     {
         $user = Auth::User();
         $building = $user->building;
-        $maintenances = $building->maintenances()->orderBy('date','desc')->get();
+        $maintenances = $building->maintenance_payments()->orderBy('date','desc')->get();
         return view('admin.account.maintenance.manage_maintenance',compact('maintenances'));
     }
 

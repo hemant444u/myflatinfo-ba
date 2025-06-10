354d96bd9f3b0560a4b0aff84aa80393fe3233d2
hemant444u <krguptahemant@gmail.com>
2025-06-10 15:56:12 +0530

    ddd

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -73,7 +73,7 @@ class Kernel extends HttpKernel
         'department' => \App\Http\Middleware\DepartmentMiddleware::class,
         'noticeboard' => \App\Http\Middleware\NoticeboardMiddleware::class,
         'event' => \App\Http\Middleware\EventMiddleware::class,
-        'classified' => \App\Http\Classified\NoticeboardMiddleware::class,
+        'classified' => \App\Http\Middleware\ClassifieddMiddleware::class,
         //'cors' => \App\Http\Middleware\CorsMiddleware::class,
     ];
 }

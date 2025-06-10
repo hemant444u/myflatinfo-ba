9b8e2a53702144830a7c0dc5f400b7f2d2ad76ae
hemant444u <krguptahemant@gmail.com>
2025-06-10 14:02:55 +0530

    ddd

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -113,7 +113,7 @@
 
 
 .table {
-  font-size: 13px; /* adjust as needed: 12px or 13px is common for tables */
+  font-size: 16px; /* adjust as needed: 12px or 13px is common for tables */
 }
 
 .table th,

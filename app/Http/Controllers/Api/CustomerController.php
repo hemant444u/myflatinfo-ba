45881f579bd72f0cd454bce4465d58bb1fb9f0c2
hemant444u <krguptahemant@gmail.com>
2025-06-11 12:48:32 +0530

    fff

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -2304,6 +2304,34 @@ class CustomerController extends Controller
             $essential->flat = $flat;
             return $essential;
         });
+        foreach($essentials as $essential){
+            foreach($essential->payments as $essential_payment){
+                $dues_amount = $essential_payment->dues_amount;
+                $late_fine = 0;
+                $gst = $essential->gst;
+                if ($essential->due_date < now()) {
+                    $late_days = now()->diffInDays(Carbon::parse($essential->due_date));
+                    switch ($essential->late_fine_type) {
+                        case 'Daily':
+                            $late_fine = $late_days * $essential->late_fine_value;
+                            break;
+                        case 'Fixed':
+                            $late_fine = $essential->late_fine_value;
+                            break;
+                        case 'Percentage':
+                            $late_fine = ($payment->dues_amount * $essential->late_fine_value) / 100;
+                            break;
+                        }
+                }
+                $total_amount = $dues_amount + $late_fine;
+                $total_gst = $total_amount * $gst / 100;
+                $grand_total = $total_amount + $total_gst;
+
+                $essential_payment->total_amount = $total_amount;
+                $essential_payment->total_gst = $total_gst;
+                $essential_payment->grand_total = $grand_total;
+            }
+        }
         return response()->json([
                 'essentials' => $essentials,
         ],200);
